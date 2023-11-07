53428d95ec511caab43ef58eaa14f6c32c197440
asnwh <asalwuhayd@hotmail.com>
2023-11-07 07:46:04 +0300

    W5-D2-HW2

---
+++ script.js
@@ -0,0 +1,29 @@
+let catph = document.getElementById("photocat")
+let hphoto = document.getElementById("headerphoto")
+let hphoto1 = document.getElementById("photodog")
+let hedtext = document.getElementById("headerdog")
+let hedtext1= document.getElementById("headerdog1")
+let hedtext2 = document.getElementById("headerdog2")
+let text1= document.getElementById("dog1")
+let text2= document.getElementById("dog2")
+let text3= document.getElementById("dog3")
+let text4= document.getElementById("dog4")
+let text5= document.getElementById("dog5")
+let text6= document.getElementById("dog6")
+let text7= document.getElementById("dog7")
+
+
+catph.addEventListener("click", function catf() {
+    hphoto.src='https://images.pexels.com/photos/16395148/pexels-photo-16395148/free-photo-of-bengal-cat-holding-a-paw-on-a-mans-head.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    hphoto1.src='https://images.pexels.com/photos/18936889/pexels-photo-18936889/free-photo-of-cat-by-vase-with-red-gerberas-on-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    hedtext.innerText="We're the Cat People"
+    hedtext1.innerText= "Book trusted, local pet sitters and walkers who will care for your cat like you would."
+    hedtext2.innerText="Services for every cat and dog"
+    text1.innerText="Cat Boarding"
+    text2.innerText="Cat walng"
+    text3.innerText="Whenever ykiour cat needs a walk."
+    text4.innerText="Catty Day Care"
+    text5.innerText="Daytime pet care in your sitter’s cat-friendly home."
+    text6.innerText="My Rover sitter sent me updates throughout the day and took care of my cat as if she were her own."
+    text7.innerText="We’re the treat-your-cat-like-family cat people"
+})
